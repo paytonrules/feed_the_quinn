bd806e1ace569681bbaa6e235632c383d93d4a4f
Eric Smith <eric@8thlight.com>
2011-12-16 15:35:24 -0600

    Load the start button into the quasi global so the assets can get to it - this clearly is not how to move forward

--- script/feed_the_quinn.js
+++ script/feed_the_quinn.js
@@ -1,6 +1,8 @@
 var Eskimo = require('eskimo');
 var Updater = require('./feed_the_quinn/updater');
 var Assets = require('./feed_the_quinn/assets');
+var global = global || window;
+global.StartButton = require('./feed_the_quinn/start_button');
 
 Eskimo({updater: Updater,
          jquery: $}).start({canvas: $('#feed_the_quinn'),
