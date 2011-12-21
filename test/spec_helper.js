de135a500cc2c710ea213ca5e6e362b6a2b6b4bd
Eric Smith <eric@8thlight.com>
2011-12-20 20:41:31 -0600

    I dont use spec helper anymore

--- test/spec_helper.js
+++
@@ -1,5 +0,0 @@
-global.FeedTheQuinn = {}; // Temp
-require("../script/feed_the_quinn/updater");
-require("../script/feed_the_quinn/state_machine");
-require("../script/feed_the_quinn/title_screen");
-exports.FeedTheQuinn = global.FeedTheQuinn;
