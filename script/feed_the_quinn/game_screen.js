fc56924bcdaa6a9bca90026de765dc896ce45a23
Eric Smith <eric@8thlight.com>
2012-01-17 18:31:43 -0600

    Making images kinda look right

--- script/feed_the_quinn/game_screen.js
+++ script/feed_the_quinn/game_screen.js
@@ -8,13 +8,11 @@ module.exports = {
   update: function() {},
 
   draw: function(screen) {
-    /*
-     *var floor = level.gameObject('floor');
-     *screen.put(Image('floor', floor.location.x, floor.location.y));
-     *var daddy = level.gameObject('daddy');
-     *screen.put(Image('daddy', daddy.location.x, floor.location.y));
-     */
+    var floor = level.gameObject('floor');
+    screen.put(Image('floor', floor.location.x, floor.location.y));
+    var daddy = level.gameObject('daddy');
+    screen.put(Image('daddy', daddy.location.x, floor.location.y));
     var baby = level.gameObject('baby');
-    screen.put(Image('baby', baby.location.x, baby.location.y));
+    screen.put(Image('mario', baby.location.x, baby.location.y));
   }
 };
