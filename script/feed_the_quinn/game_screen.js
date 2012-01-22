092237eac206bf8e09f51167bfcf24925811596b
Eric Smith <eric@8thlight.com>
2012-01-21 20:09:18 -0600

    Starting to move the king left

--- script/feed_the_quinn/game_screen.js
+++ script/feed_the_quinn/game_screen.js
@@ -1,16 +1,33 @@
-var level = require('eskimo').Level;
-var Image = require('eskimo').Image;
+var level = require('eskimo').Level,
+    Image = require('eskimo').Image,
+    currentVelocity = 0,
+    daddy;
+
+function daddy() {
+  return level.gameObject('daddy');
+}
+
 module.exports = {
+  PLAYER_VELOCITY: 5,
   load: function() {
     level.load('levelOne');
   },
 
-  update: function() {},
+  update: function() {
+    daddy().location.x += currentVelocity;
+  },
 
+  moveLeft: function() {
+    if (currentVelocity == 0) {
+      currentVelocity -= this.PLAYER_VELOCITY;
+    }
+  },
+
+  // Eventually want to remove the idea of draw methods in favor
+  // of scenes and visible objects
   draw: function(screen) {
     var floor = level.gameObject('floor');
     screen.put(Image('floor', floor.location.x, floor.location.y));
-    var daddy = level.gameObject('daddy');
     screen.put(Image('daddy', daddy.location.x, floor.location.y));
     var baby = level.gameObject('baby');
     screen.put(Image('mario', baby.location.x, baby.location.y));
--- test/feed_the_quinn/game_screen_spec.js
+++ test/feed_the_quinn/game_screen_spec.js
@@ -1,18 +1,47 @@
 
 describe("FeedTheQuinn#GameScreen", function() {
   var sandbox = require('sinon').sandbox.create(),
-      level = require('eskimo').Level; 
+      eskimo = require('eskimo'),
+      level = eskimo.Level,
+      KeyCodes = eskimo.KeyCodes,
+      game = require('../../script/feed_the_quinn/game_screen');
 
   afterEach(function() {
     sandbox.restore();
   });
 
   it("loads the levelOne on load", function() {
-    var levelLoad = sandbox.stub(level, 'load'),
-        game = require('../../script/feed_the_quinn/game_screen');
+    var levelLoad = sandbox.stub(level, 'load');
 
     game.load();
 
     levelLoad.calledWith('levelOne').should.be.true;
+  });
+
+  it("moves the daddy object PLAYER_VELOCITY left", function() {
+    level.initializeAssets(require('jquery'));
+    level.levels = { 'levelOne' : {}};
+    level.load('levelOne');
+    level.addGameObject('daddy', {location: {x: 10}});
+    game.moveLeft();
+    game.update();
+
+    var daddy = level.gameObject("daddy");
+
+    daddy.location.x.should.eql(10 - game.PLAYER_VELOCITY);
+  });
+
+  it("keeps moving the daddy object left until stop moving left is sent", function() {
+    level.initializeAssets(require('jquery'));
+    level.levels = { 'levelOne' : {}};
+    level.load('levelOne');
+    level.addGameObject('daddy', {location: {x: 10}});
+    game.moveLeft();
+    game.update();
+    game.update();
+    
+    var daddy = level.gameObject("daddy");
+
+    daddy.location.x.should.eql(10 - (2 * game.PLAYER_VELOCITY));
   });
 });
