e81ff35a83d346a45cc24d6cae22abca6cbad812
Eric Smith <eric@8thlight.com>
2012-07-16 07:37:53 -0500

    Update dependencies file, and add a run specs, to the jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,5 @@
 function sh(command) {
-  var sys = require('util')
+  var sys = require('util');
   var exec = require('child_process').exec;
   function puts(error, stdout, stderr) { sys.puts(stdout); }
   exec(command, puts);
@@ -11,12 +11,19 @@ function npm_install() {
   }
 }
 
-desc("Browserify the project (no minification")
+desc("Browserify the project (no minification");
 task("browserify", function() {
   sh('browserify script/feed_the_quinn.js > script/feed_the_quinn_browserified.js');
 }); 
 
-desc("Install node module dependencies")
+desc("Run specs");
+task("spec", function() {
+  sh('. run_specs');
+});
+
+
+desc("Install node module dependencies");
 task("dependencies", function() {
-  npm_install('should', 'mocha -g', 'underscore');
+  npm_install('browserify', 'canvas', 'jsdom', 'jquery', 'mocha -g', 'should', 'sinon', 'underscore');
+  // Linik to eskimo, not sure how to do that
 });
