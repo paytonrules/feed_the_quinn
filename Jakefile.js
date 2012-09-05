95f6b083ddaa8703f3aecca18f2e9d32b38aee06
Eric Smith <paytonrules@gmail.com>
2012-09-05 07:45:29 -0500

    Cleanup the Jakefile to use jake exec

--- Jakefile.js
+++ Jakefile.js
@@ -1,29 +1,25 @@
-function sh(command) {
-  var sys = require('util');
-  var exec = require('child_process').exec;
-  function puts(error, stdout, stderr) { sys.puts(stdout); }
-  exec(command, puts);
-}
-
 function npm_install() {
-  for(var i=0; i < arguments.length; i++) {
-    sh("npm install " + arguments[0]);
+  for(var i = 0; i < arguments.length; i++) {
+    jake.exec("npm install " + arguments[i], function() {}, {printStderr: true});
   }
 }
 
 desc("Browserify the project (no minification");
 task("browserify", function() {
-  sh('browserify script/feed_the_quinn.js > script/feed_the_quinn_browserified.js');
+  jake.exec('browserify script/feed_the_quinn.js > script/feed_the_quinn_browserified.js', function() {
+    console.log("browser version built");
+    complete();
+  }, {printStderr: true});
 }); 
 
 desc("Run specs");
 task("spec", function() {
-  sh('. run_specs');
+  jake.exec('. run_specs', function() {}, {printStdout: true, printStderr: true});
 });
 
 
 desc("Install node module dependencies");
 task("dependencies", function() {
-  npm_install('browserify', 'canvas', 'jsdom', 'jquery', 'mocha -g', 'should', 'sinon', 'underscore');
-  // Linik to eskimo, not sure how to do that
+  npm_install('browserify -g', 'canvas', 'jsdom', 'jquery', 'mocha -g', 'should', 'sinon', 'underscore');
+  jake.exec("npm link eskimo", function() {}, {printStderr: true});
 });
