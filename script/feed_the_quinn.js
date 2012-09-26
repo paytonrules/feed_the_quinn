d1339dfc9462e1b3cfbd0cb1fa82c01b9a582f90
Eric Smith <paytonrules@gmail.com>
2012-09-25 22:21:44 -0500

    Dont send in jquery anymore

--- script/feed_the_quinn.js
+++ script/feed_the_quinn.js
@@ -2,8 +2,7 @@ var Eskimo = require('eskimo');
 var game = require('./feed_the_quinn/feed_the_quinn');
 var Assets = require('./feed_the_quinn/assets');
 
-Eskimo({game: game,
-         jquery: $}).start({canvas: $('#feed_the_quinn'),
+Eskimo({game: game}).start({canvas: $('#feed_the_quinn'),
                             document: document,
-                            levels: Assets // Overloaded term
+                            levels: Assets // Overloaded term - use levels
                            });
