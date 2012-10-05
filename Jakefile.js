77f0de9a45e30c1a9e453a4186a9d948d6373f42
Eric Smith <paytonrules@gmail.com>
2012-10-05 08:49:27 -0500

    Now working with onejs and npm test

--- Jakefile.js
+++ Jakefile.js
@@ -1,25 +1,13 @@
-function npm_install() {
-  for(var i = 0; i < arguments.length; i++) {
-    jake.exec("npm install " + arguments[i], function() {}, {printStderr: true});
-  }
-}
-
-desc("Browserify the project (no minification");
-task("browserify", function() {
-  jake.exec('browserify script/feed_the_quinn.js > script/feed_the_quinn_browserified.js', function() {
+desc("Bundle the project (no minification");
+task("bundle", function() {
+  jake.exec('onejs build package.json script/feed_the_quinn_bundle.js', function() {
     console.log("browser version built");
     complete();
   }, {printStderr: true});
 }); 
 
-desc("Run specs");
-task("spec", function() {
-  jake.exec('. run_specs', function() {}, {printStdout: true, printStderr: true});
-});
-
-
 desc("Install node module dependencies");
 task("dependencies", function() {
-  npm_install('browserify -g', 'canvas', 'jsdom', 'jquery', 'mocha -g', 'should', 'sinon', 'underscore');
+  npm_install('onejs -g', 'canvas', 'jsdom', 'jquery', 'mocha -g', 'should', 'sinon', 'underscore');
   jake.exec("npm link eskimo", function() {}, {printStderr: true});
 });
--- script/feed_the_quinn/index.js
+++ script/feed_the_quinn/index.js
@@ -1,6 +1,7 @@
 var Eskimo = require('eskimo');
 var game = require('./feed_the_quinn.js');
 var Levels = require('./assets.js');
+var $ = require('jquery');
 
 Eskimo({game: game}).start({canvas: $('#feed_the_quinn'),
                             document: document,
