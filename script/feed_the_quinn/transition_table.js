112b7c20ed4a877fe72d713cc2d801c2655450eb
paytonrules <esmith@8thlight.com>
2012-12-18 08:36:48 -0600

    Green Tests!
    Moving to the newer and cleaner version of testing the game spec,
    as well as using my new state machine.

---
+++ script/feed_the_quinn/transition_table.js
@@ -0,0 +1,11 @@
+var TitleScreen = require("./title_screen");
+var GameScreen = require("./game_screen");
+
+module.exports = [
+  [TitleScreen, "startGame", GameScreen],
+  [TitleScreen, "update", TitleScreen, "update"],
+  [TitleScreen, "click", TitleScreen, "click"],
+  [GameScreen, "update", GameScreen, "update"],
+  [GameScreen, "keydown", GameScreen, "keydown"]
+  [GameScreen, "keyup", GameScreen, "keyup"]
+];
